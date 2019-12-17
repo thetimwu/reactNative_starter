d545e7d527764f35cc4cd242305a350c8b3f3b4a
Tim WU <timwuuk@gmail.com>
2019-12-17 15:50:27 +0000

    add input component with styles, add button functions in App

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -1,24 +1,72 @@
-import React from "react";
-import { View, Text, StyleSheet, TextInput, Button } from "react-native";
+import React, { useState } from "react";
+import {
+  View,
+  Text,
+  StyleSheet,
+  Button,
+  TouchableWithoutFeedback,
+  Keyboard
+} from "react-native";
 import Card from "../components/Card";
+import Colors from "../constants/color";
+import Input from "../components/Input";
 
 const StartGameScreen = props => {
+  const [enterValue, setEnterValue] = useState("");
+  const [confirm, setConfirm] = useState(false);
+  const [selectedNumber, setSelectedNumber] = useState();
+
+  const numberInputHandler = inputText => {
+    setEnterValue(inputText.replace(/[^0-9]/, ""));
+  };
+
+  const resetInputHandler = () => {
+    setEnterValue("");
+    setConfirm(false);
+  };
+
+  const confirmInputHandler = () => {
+    setConfirm(true);
+    setSelectedNumber(parseInt(enterValue));
+    setEnterValue("");
+  };
+
   return (
-    <View style={styles.screen}>
-      <Text style={styles.title}>Start a New Game</Text>
-      <Card style={styles.inputContainer}>
-        <Text>Select a number</Text>
-        <TextInput />
-        <View style={styles.buttonContainer}>
-          <View style={styles.myButton}>
-            <Button title="Reset" onPress={() => {}} color="#c717fc" />
+    <TouchableWithoutFeedback onPress={() => Keyboard.dismiss()}>
+      <View style={styles.screen}>
+        <Text style={styles.title}>Start a New Game</Text>
+        <Card style={styles.inputContainer}>
+          <Text>Select a number</Text>
+          <Input
+            style={styles.input}
+            blurOnSubmit
+            autoCapitalize="none"
+            autoCorrect={false}
+            keyboardType="number-pad"
+            maxLength={2}
+            onChangeText={numberInputHandler}
+            value={enterValue}
+          />
+          <View style={styles.buttonContainer}>
+            <View style={styles.myButton}>
+              <Button
+                title="Reset"
+                onPress={resetInputHandler}
+                color={Colors.accent}
+              />
+            </View>
+            <View style={styles.myButton}>
+              <Button
+                title="Confirm"
+                onPress={() => {}}
+                color={Colors.primary}
+                onPress={confirmInputHandler}
+              />
+            </View>
           </View>
-          <View style={styles.myButton}>
-            <Button title="Confirm" onPress={() => {}} color="#f7287b" />
-          </View>
-        </View>
-      </Card>
-    </View>
+        </Card>
+      </View>
+    </TouchableWithoutFeedback>
   );
 };
 
@@ -45,6 +93,10 @@ const styles = StyleSheet.create({
   },
   myButton: {
     width: "40%"
+  },
+  input: {
+    width: 50,
+    textAlign: "center"
   }
 });
 
